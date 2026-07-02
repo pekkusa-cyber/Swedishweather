2de823f5b6267a01ce6d40b733ee7ec715d68513
pekkusa-cyber <pekkusa@gmail.com>
2026-07-03 01:09:34 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'vaderrad-v7';
+const SHELL = ['index.html', 'manifest.webmanifest', 'clock-bg.webp', 'icon-192.png', 'icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
